3ce397d1ff4ca581ea7178427e571a4dbd0d168f
Om Nath Kushwaha <58984004+omkushwaha14@users.noreply.github.com>
2020-09-25 19:02:54 +0545

    Update 3.kadenns algorithms.cpp

--- 3.kadenns algorithms.cpp
+++ 3.kadenns algorithms.cpp
@@ -1,48 +1,44 @@
 #include <iostream>
 using namespace std;
 
- int  maxsum(int a[],int n){
-     int max_total=0;
-    int max_end=0;
-    int start =0, end = 0, s=0;
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int a[n];
 
-     for(int i=0;i<n;i++){
-      max_end= max_end+a[i];
-      if(max_end<a[i]){
-          max_end=0;
-          s=i+1;
-      }
-      if(max_total<max_end){
-          max_total=max_end;
-          start = s;
-          end = i;
-      }
+        for(int i=0;i<n;i++){
+            cin>>a[i];
+        }
+        int maxsum=0;
+        int totalsum=0;
+
+        for(int i=0;i<n;i++){
+           maxsum=maxsum+a[i];
+           if(maxsum<a[i]){
+               maxsum=0;
+           }
+           if(totalsum<maxsum){
+               totalsum=maxsum;
+
+           }
+
+        }
+        cout<<"output:"<<totalsum;
+
 
     }
-     cout<<start<<" "<<end<<endl;
-    return max_total;
+
 }
 
-
-int main() {
-     int a[100];
-     int n;
-     cin>>n;
- for(int i=0;i<n;i++){
-     cin>>a[i];
- }
-   int max_sum= maxsum(a,n);
- cout<<max_sum;
-}
-
-
-
-
-////answers///
-7
-input:  -2 -3 4 -1 -2 1 5 -3
-
-output:
-2 6
-7
+////output//
+2
+4
+1 2 -4 -3
+output:3
+4
+1 2 -4 -3
+output:3
 Process finished with exit code 0
