8989744978b5df3c0a200b53999cd52d22742b61
Om Nath Kushwaha <58984004+omkushwaha14@users.noreply.github.com>
2020-09-28 23:15:44 +0545

    Create 6.Inversion of array unoptimized.cpp

---
+++ 6.Inversion of array unoptimized.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+int t;
+cin>>t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int a[n];
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (a[i] > a[j]) {
+                    count++;
+                 
+                }
+
+
+            }
+            
+
+        }
+           cout<<count;
+       
+         
+    }
+    
+}
+
+/////Answers//////
+1
+42
+468 335 1 170 225 479 359 463 465 206 146 282 328 462 492 496 443 328 437 392 105 403 154 293 383 422 217 219 396 448 227 272 39 370 413 168 300 36 395 204 312 323
+494
+Process finished with exit code 0
